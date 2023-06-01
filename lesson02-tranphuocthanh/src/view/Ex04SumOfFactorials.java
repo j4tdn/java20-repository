e4a02afa3de9078ec1936fecfa1201f248a51cdc
Thanh <thanhadp2402@gmail.com>
2023-06-01 22:07:33 +0700

    lesson02-tranphuocthanh 06.01.2023

---
+++ lesson02-tranphuocthanh/src/view/Ex04SumOfFactorials.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.Random;
+
+public class Ex04SumOfFactorials {
+	public static void main(String[] args) {
+		Random rd = new Random();
+		long sum = 0, tmp;
+		int count = 0;
+		while (count < 4) {
+			int number = rd.nextInt(10, 21);
+			tmp = factorial(number);
+			System.out.println(number + "! = " + tmp);
+			sum += tmp;
+			count++;
+		}
+		System.out.println("sum: " + sum);
+	}
+
+	private static long factorial(int number) {
+		long result = 1;
+		for (int i = 1; i <= number; i++) {
+			result *= i;
+		}
+
+		return result;
+	}
+}
---
+++ lesson02-tranphuocthanh/src/view/Ex06MinMax.java
@@ -0,0 +1,42 @@
+package view;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex06MinMax {
+	private static int d = 1;
+
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		int count = 0;
+		int[] numbers = new int[3];
+		do {
+			System.out.print(d++ + ".enter a number: ");
+			String check = ip.nextLine();
+			if (check.matches("[0-9]|1[0-9]")) {
+				int number = Integer.parseInt(check);
+				numbers[count++] = number;
+			} else {
+				System.out.println("invalid number, plz enter again!!!");
+			}
+		} while (count < 3);
+
+		int[] result = getMinMax(numbers);
+		System.out.println("min: " + result[0]);
+		System.out.println("max: " + result[1]);
+	}
+
+	private static int[] getMinMax(int[] numbers) {
+		int min, max;
+		min = max = numbers[0];
+
+		for (int i = 0; i < numbers.length; i++) {
+			if (min > numbers[i])
+				min = numbers[i];
+			if (max < numbers[i])
+				max = numbers[i];
+		}
+
+		return new int[] { min, max };
+	}
+}
