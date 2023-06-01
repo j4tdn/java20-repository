5c84a24c66e63f5f57a659c5f9291ff1770ea68d
Thanh <thanhadp2402@gmail.com>
2023-06-01 22:07:33 +0700

    lesson02-tranphuocthanh 06.01.2023

---
+++ lesson02-tranphuocthanh/src/view/Ex08IsPrimeNumber.java
@@ -0,0 +1,33 @@
+package view;
+
+import java.util.Scanner;
+
+public class Ex08IsPrimeNumber {
+	private static int d = 1;
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		int count = 0;
+		do {
+			System.out.print(d++ + ".enter a number: ");
+			String check = ip.nextLine();
+			if (check.matches("\\d+")) {
+				int number = Integer.parseInt(check);
+				System.out.println(isPrimeNumber(number));
+				break;
+			} else {
+				System.out.println("invalid number, plz enter again!!!");
+				count++;
+			}
+		} while (count < 5);
+		System.out.println("finished!");
+	}
+	
+	private static boolean isPrimeNumber(int number) {
+		if (number == 1) return false;
+		for (int i = 2; i <= Math.sqrt(number); i++) {
+			if (number % i == 0) return false;
+		}
+		
+		return true;
+	}
+}
