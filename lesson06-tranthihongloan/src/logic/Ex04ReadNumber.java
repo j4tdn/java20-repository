807cb9b8ea02b902a09b3c04f5df00a506c4be21
hongloan0115 <hongloan0115@gmail.com>
2023-08-02 21:45:46 +0700

    lesson06-tranthihongloan 02.08.2023

--- lesson06-tranthihongloan/src/logic/Ex04ReadNumber.java
+++ lesson06-tranthihongloan/src/logic/Ex04ReadNumber.java
@@ -5,62 +5,86 @@ import java.util.Scanner;
 public class Ex04ReadNumber {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int n = Integer.parseInt(sc.nextLine());
-		System.out.println(readNumber(n));
-
-		sc.close();
+		int n;
+		System.out.println("Nhập một số: ");
+		
+		while(true) {
+			try {
+				n = Integer.parseInt(sc.nextLine());
+				System.out.println(readNumber(n));
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				System.out.println("Nhập sai kiểu dữ liệu.");
+				System.out.println("Nhập lại:");
+			} 
+		}
 	}
 
 	public static String readNumber(int n) {
 		String st = "";
-		int[] arr = new int[3];
+
+		// Đếm có bao nhiêu chữ số
+		int[] arr = new int[countCharacter(n)];
+
+		// Tách từng chữ số thành mảng
+		arr = separateDigits(n, arr);
+
+		for (int j = 0; j <= arr.length - 1; j++) {
+			switch (arr[j]) {
+			case 0:
+				st = "không" + st;
+				break;
+			case 1:
+				st = "một" + st;
+				break;
+			case 2:
+				st = "hai" + st;
+				break;
+			case 3:
+				st = "ba" + st;
+				break;
+			case 4:
+				st = "bốn" + st;
+				break;
+			case 5:
+				st = "năm" + st;
+				break;
+			case 6:
+				st = "sáu" + st;
+				break;
+			case 7:
+				st = "bảy" + st;
+				break;
+			case 8:
+				st = "tám" + st;
+				break;
+			case 9:
+				st = "chín" + st;
+				break;
+			}
+
+			if (j == 0 && (arr.length == 2 || arr.length == 3)) {
+				st = " mươi " + st;
+			}
+			if (j == 1 && arr.length == 3) {
+				st = " trăm " + st;
+			}
+		}
+		return st;
+	}
+
+	public static int countCharacter(int n) {
+		String st = "" + n;
+		return st.length();
+	}
+
+	public static int[] separateDigits(int n, int[] arr) {
 		int i = 0;
-		while (n != 0 && i < 4) {
+		while (n != 0 && i < arr.length) {
 			arr[i] = n % 10;
 			n = n / 10;
 			i++;
 		}
-		for (int j = arr.length - 1; j >= 0; j--) {
-			switch (arr[j]) {
-			case 0:
-				st = st + "không";
-				break;
-			case 1:
-				st = st + "một";
-				break;
-			case 2:
-				st = st + "hai";
-				break;
-			case 3:
-				st = st + "ba";
-				break;
-			case 4:
-				st = st + "bốn";
-				break;
-			case 5:
-				st = st + "năm";
-				break;
-			case 6:
-				st = st + "sáu";
-				break;
-			case 7:
-				st = st + "bảy";
-				break;
-			case 8:
-				st = st + "tám";
-				break;
-			case 9:
-				st = st + "chín";
-				break;
-			}
-			if(j == 2) {
-				st = st + " trăm ";
-			}
-			if(j == 1) {
-				st = st + " mươi ";
-			}
-		}
-		return st;
+		return arr;
 	}
 }
