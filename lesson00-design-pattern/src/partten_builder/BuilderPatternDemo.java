78e9a119e16cc74d3fe6e6d961cd08979c7c30c1
Anh Nga <nguyenanhnga11@gmail.com>
2023-08-04 22:44:16 +0700

    lesson07-libraries 04.08.2023

---
+++ lesson00-design-pattern/src/partten_builder/BuilderPatternDemo.java
@@ -0,0 +1,74 @@
+package partten_builder;
+
+import bean.User;
+import bean.User.Builder;
+import bean.UserAdv;
+import bean.UserHigherAdv;
+
+public class BuilderPatternDemo {
+	
+	/**
+	 Builder Pattern is a construction pattern
+	 + build/create complex objects(has many attributes)
+	 + build out an immutable(impossible to update data at HEAP) object
+	 + create one construction process replace for many representations(many overload constructors)
+	 
+	 + students(100 elements H1) --> students(point > 7)
+	 + --> immutable object
+	 + remove point <= 7 students(70 elements H1)
+	 
+	 + immutable
+	   . create many constructors with enough parameters required
+	     a, b
+	     a, c
+	     a, b, c
+	     b, c
+	     --> too many constructor
+	     --> cannot create duplicate constructor with same data type & amount
+	 
+	 + builder pattern --> create just one construction to create an immutable object
+	 */
+	public static void main(String[] args) {
+		
+		// required: firstName, lastName, phone
+		User u1 = User.builder()
+				.withFirstName("Le")
+				.withLastName("Teo")
+				.withPhone("2345678")
+				.build();
+		
+		System.out.println("u1 --> " + u1);
+		
+		// required: firstName, age, address
+		User u2 = User.builder()
+				.withFirstName("Le")
+				.withAge(20)
+				.withAddress("Da Nang")
+				.build();
+		
+		System.out.println("u2 --> " + u2);
+		
+		// required: firstName, lastName, address
+		UserAdv ua1 = UserAdv.of()
+				.firstName("Hoang")
+				.lastName("Teo")
+				.address("Viet Nam");
+		
+		ua1.firstName("Hoang Updated");
+		System.out.println("ua1 --> " + ua1);
+		
+		// required: firstName, lastName, address
+		UserHigherAdv uha1 = UserHigherAdv.of()
+				.firstName("Le")
+				.lastName("Ben")
+				.address("Viet Nam");
+		
+		System.out.println("uha1 address --> " + System.identityHashCode(uha1));
+		System.out.println("uha1 --> " + uha1);
+		
+		UserHigherAdv uha2 = uha1.lastName("Benjamin");
+		
+		System.out.println("uha2 address --> " + System.identityHashCode(uha2));
+		System.out.println("uha2 --> " + uha2);
+	}
+}
---
+++ lesson06-exception/src/c/finallyz/Ex01TryCatchFinallyDemo.java
@@ -0,0 +1,66 @@
+package c.finallyz;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Ex01TryCatchFinallyDemo {
+	public static void main(String[] args) {
+		String[] employees = mockData();
+		File file = new File("data.txt");
+		
+		System.out.println("** __ Start writing file ... __ **");
+		
+		try {
+			writeFile(file, employees);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("** __ Finished ... __ **");
+	}
+	
+	private static void writeFile(File file, String[] employees) throws IOException {
+		if (!file.exists()) {
+			throw new FileNotFoundException("File " + file.getName() + " is not existed !!!");
+		}
+		
+		FileWriter fw = null;
+		
+		try {
+			// open-connect to the file
+			fw = new FileWriter(file);
+			for (String employee: employees) {
+				String[] tokens = employee.split(", ");
+				int age = Integer.parseInt(tokens[2]);
+				int salaryFactor = (age - 18) * 2;
+				
+				String line = tokens[0] + ", " + tokens[1] + ", " + salaryFactor;
+				fw.write(line + "\n");
+				
+				System.out.println("LOG info >> write content '" + line + "' to file " + file.getName());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fw != null) {
+				System.out.println("closing file " + file.getName());
+				fw.close();
+			}
+		}
+	}
+	
+	private static String[] mockData() {
+		return new String[] {
+				"1, Le Na, 28",
+				"2, Le Teo, 19",
+				"3, Hoang Ban, 20",
+				"4, Nguyen Teo, 26",
+				"5, Pham Van, 26",
+				"6, Le Van, 3o",
+				"7, Le Cong, 20",
+				"8, Le Nam Teo, 19"
+		};
+	}
+}
