32e6ec0bc69772761a56571051729c46a305c9b6
Quyen Phan <Quyen.Phan@mgm-tp.com>
2023-08-07 11:32:10 +0700

    lesson07-libraries 07.08.2023

---
+++ lesson00-design-pattern/src/pattern/singleton/SingletonPatternDemo.java
@@ -0,0 +1,21 @@
+package pattern.singleton;
+
+public class SingletonPatternDemo {
+	
+	public static void main(String[] args) {
+		
+		Singleton s1 = Singleton.getInstance();
+		s1.setValue(567);
+		
+		System.out.println("s1 value --> " + s1.getValue());
+		System.out.println("s1 hash --> " + System.identityHashCode(s1));
+		
+		System.out.println("\n ------ ------ ------ \n");
+		
+		Singleton s2 = Singleton.getInstance();
+		System.out.println("s2 value --> " + s2.getValue());
+		System.out.println("s2 hash --> " + System.identityHashCode(s2));
+		
+		// Calendar: code style giống Singleton
+	}
+}
