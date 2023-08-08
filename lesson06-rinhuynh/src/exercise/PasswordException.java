e179ca338139156b6883062bc1e0b517fb381ab8
rinhuynh <huynhthanhrin99@gmail.com>
2023-08-08 21:04:05 +0700

    lesson06-huynhthanhrin 08.08.2023

---
+++ lesson06-rinhuynh/src/exercise/PasswordException.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class PasswordException extends Exception {
+
+
+	private static final long serialVersionUID = 8940121081553392476L;
+
+	public PasswordException(String message) {
+		super(message);
+	}
+}
