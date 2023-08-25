14d146d98c8999a3241d101863a159f11afc8ff6
hongloan0115 <hongloan0115@gmail.com>
2023-08-25 19:46:45 +0700

    lesson08-string 25.08.2023

--- lesson06-exception/src/a/validate_trycatch_throw_throws/Ex03BasicDemo_Throw.java
+++ lesson06-exception/src/a/validate_trycatch_throw_throws/Ex03BasicDemo_Throw.java
@@ -2,19 +2,19 @@ package a.validate_trycatch_throw_throws;
 
 public class Ex03BasicDemo_Throw {
 	public static void main(String[] args) {
-		// validate: calidate trước đoạn code bị exception
+		// validation: validate trước đoạn code bị exception
 		// try catch: đưa đoạn code có khả năng bị exception vào khối try
 		
 		// throw_throws:
 		// throw: ném exception trước khi gọi đoạn code xảy ra lỗi
-		// throws: ném tại vị trí khai báo hàm chứa đoạn code bị exception
+		// throws: ném tại vị trí khai báo hàm(chứa đoạn code bị exception)
 		//       : để chỗ gọi hàm phải biết và xử lý exception
 		
-		System.out.println("**__Demo Throw Throws __**\n");
+		System.out.println("**__ Demo Throw Throws __**\n");
 		
-		// validate: require input b!=0 before calling divide method
+		// validate: require input b != 0 before calling divide method
 		
-		// validate
+		// validated
 		System.out.println("divide(8, 2) --> " + divide(8, 2) + "\n");
 		
 		// invalidate
@@ -26,14 +26,15 @@ public class Ex03BasicDemo_Throw {
 			ae.printStackTrace();
 		}
 		
-		System.out.println("**__Finished__**");
+		System.out.println("**__ Finished __**");
 	}
 	
 	private static int divide(int a, int b) {
 		// việc xảy ra ngoại lệ hay không, phụ thuộc vào tham số truyền vào
 		// validate cho b: ko bao giờ xảy ra
 		// chưa validate cho b: có thể bị ngoại lệ
-		if(b == 0) {
+		// numerator
+		if (b == 0) {
 			// failed first
 			throw new ArithmeticException("denominator should not be zero");
 		}
@@ -42,9 +43,8 @@ public class Ex03BasicDemo_Throw {
 	}
 	
 	/*
-	 java.lang.ArithmeticException: / by zero
-		at a.validate_trycatch_throw_throws.Ex03BasicDemo_Throw_Throws.divide(Ex03BasicDemo_Throw_Throws.java:23)
-		at a.validate_trycatch_throw_throws.Ex03BasicDemo_Throw_Throws.main(Ex03BasicDemo_Throw_Throws.java:17)
-
+	java.lang.ArithmeticException: / by zero
+		at a.validate_trycatch_throw_throws.Ex03BasicDemo_Throw_Throws.divide(Ex03BasicDemo_Throw_Throws.java:24)
+		at a.validate_trycatch_throw_throws.Ex03BasicDemo_Throw_Throws.main(Ex03BasicDemo_Throw_Throws.java:18)
 	 */
 }
