5826f1a6865f41addf048495324f983820ebb268
pvloc <phamvanloc2301@gmail.com>
2023-10-26 23:06:43 +0700

    lesson12-stream 26.10.2023

---
+++ lesson12-pvloc/src/view/lambda/expression/Ex01LambdaTests.java
@@ -0,0 +1,40 @@
+package view.lambda.expression;
+
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
+public class Ex01LambdaTests {
+	public static void main(String[] args) {
+		
+		/*
+		 1. () -> {} 
+		  --> đoán được công thức của hàm trừu tượng
+		  + số lượng tham số trong hàm trừu tượng của FI
+		  + kiểu trả về của hàm trừu tượng đó
+		  + tên của FI có sẵn trong Java
+		  
+		 2. () -> "Raoul"
+		 + ko có tham số truyền vào
+		 + trả về T
+		 
+		 4. (Integer i) -> return "Alan" + i;
+		 + có 1 tham số truyền vào kiểu T
+		 + trả về kiểu R
+		 */
+		Runnable runnable = () -> {};
+		runnable.run();
+		
+		Callable<String> callable = () -> "Raoul";
+		try {
+			System.out.println("callable: " + callable.call());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Function<Integer, String> function = (Integer i) -> {
+			return "Alan" + i;
+		};
+		System.out.println("function: " + function.apply(18));
+		
+	}
+}
---
+++ lesson12-pvloc/src/view/lambda/expression/Ex04ConstructorReference.java
@@ -0,0 +1,46 @@
+package view.lambda.expression;
+
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+import bean.Tuple;
+
+public class Ex04ConstructorReference {
+	
+	public static void main(String[] args) {
+		// 1. implementation class
+
+		// 2. anonymous class
+
+		// 3. anonymous function(lambda expression)
+
+		// 4. method reference
+		//    --> với bất kỳ biểu thức lambda nào
+		//    + số lượng tham số bất kỳ
+		//    + kiểu trả về bất kỳ
+		
+		// 5. constructor reference
+		//    --> lambda thỏa mãn yêu cầu
+		//    + số lượng tham số bất kỳ
+		//    + kiểu trả về KDL đối tượng và được tạo thông qua constructor
+		
+		// Supplier<Tuple> s1 = () -> new Tuple();
+		// Supplier<Tuple> s1 = Ex04ConstructorReference::init; // method reference
+		// BiFunction<Integer, Integer, Tuple> b1 = (l, r) -> new Tuple(l, r); // method reference
+		
+		// Việc new mà chọn hàm khởi tạo nào --> phụ thuộc vào kiểu functional interface
+		Supplier<Tuple> s1 = Tuple::new; // constructor reference
+		Tuple tuple = s1.get();
+		
+		BiFunction<Integer, Integer, Tuple> b1 = Tuple::new; // constructor reference
+		Tuple tupleBe = b1.apply(7, 7);
+		
+		System.out.println("tuple --> " + tuple);
+		System.out.println("tupleBe --> " + tupleBe);
+	}
+	
+	public static Tuple init() {
+		return new Tuple();
+	}
+	
+}
