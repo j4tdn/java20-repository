d30e9875003cc778369105f3687a4047335d24fc
Thanh <thanhadp2402@gmail.com>
2023-11-14 11:07:35 +0700

    lesson16-Database-SQL-DDL 14.11.2023

---
+++ 2.Database Patches/000.sql
@@ -0,0 +1,21 @@
+-- create database
+CREATE DATABASE java20_shopping CHAR SET utf8mb4;
+
+-- Để chạy
+-- 1. Bôi đoạn code --> execute
+-- 2. Để chạy 1 câu lệnh từ đầu đến ' ; '--> đạt chuột vào ctrl + enter
+
+-- 001.sql
+-- --> ITEM_GROUP, ITEM, SIZE, ITEM_DETAIL
+
+-- 002.sql
+-- --> CUSTOMER
+
+-- 003.sql
+-- --> DEPARTMENT, EMPLOYEE
+
+-- 004.sql
+-- --> PAYMENT_METHOD, ORDER_STATUS
+
+-- 005.sql
+-- --> ORDER, BILL, ORDER_DETAIL, ORDER_STATUS_DETAIL
