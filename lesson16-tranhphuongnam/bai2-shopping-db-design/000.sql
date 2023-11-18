678e82c50f6d9aa948ebe1088aaceb8ecaa478cd
BlueJava2003 <tphuongnam98@gmail.com>
2023-11-18 17:19:39 +0700

    lesson16-tranphuongnam 18.11.2023

---
+++ lesson16-tranhphuongnam/bai2-shopping-db-design/000.sql
@@ -0,0 +1,17 @@
+-- create database
+CREATE DATABASE java20_shopping CHAR SET utf8mb4;
+
+-- Để chạy câu lệnh
+-- 1. Bôi đậm đoạn code cần chạy -> nhấn nút execute
+-- 2. Để chạy 1 lệnh (từ bắt đầu tới ;) --> đặt trở chuột vào dòng lệnh muốn chạy --> nhấn Ctrl Enter
+
+-- 001.sql
+-- --> ITEM GROUP, ITEM, SIZE, ITEM DETAIL
+-- 002.sql
+-- --> CUSTOMER
+-- 003.sql
+-- --> DEPARTMENT, EMPLOYEE
+-- 004.sql
+-- --> PAYMENT METHOD, ORDER STATUS
+-- 005.sql
+-- --> ORDER, BILL, ORDER DETAIL, ORDER_STATUS_DETAIL
