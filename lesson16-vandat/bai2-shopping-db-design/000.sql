e987b421e9ea905b5e50e2f5c0911b0c422fe9ed
vdat <dinhvandat747@gmail.com>
2023-11-19 09:56:59 +0700

    lesson16-vandat 19.11.2023

---
+++ lesson16-vandat/bai2-shopping-db-design/000.sql
@@ -0,0 +1,21 @@
+-- create database
+CREATE DATABASE JAVA1920_SHOPPING CHAR SET utf8mb4;
+
+-- để chạy câu lệnh
+-- 1. bôi đậm đoạn code cần chạy -> nhấn nút execute
+-- 2. để chạy 1 lệnh (từ bắt đầu đến ;) -> đặt trỏ ở dòng cần chạy -> nhấn Ctrl + Enter
+
+-- 001.sql
+-- -> ITEM_GROUP, ITEM, SIZE, ITEM_DETAIL
+
+-- 002.sql
+-- -> CUSTOMER
+
+-- 003.sql
+-- -> DEPARTMENT, EMPLOYEE
+
+-- 004.sql
+-- -> PAYMENT_METHOD, ORDER_STATUS
+
+-- 005.sql
+-- -> ORDER, BILL, ORDER_DETAIL, ORDER_STATUS_DETAIL
