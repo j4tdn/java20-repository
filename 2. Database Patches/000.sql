76f7093cf6745e5154eea2f8134333d057e47d19
nuynt <tranxuannguyen732@gmail.com>
2023-11-23 18:08:12 +0700

    lesson16-rdbms-23.11.2023

---
+++ 2. Database Patches/000.sql
@@ -0,0 +1,21 @@
+-- create database
+CREATE DATABASE JAVA20_SHOPPING CHAR SET utf8mb4;
+
+-- để chạy câu lệnh
+-- 1. Bôi đậm đoạn code cần chạy -- nhấn nút execute
+-- 2. Để chạy 1 lệnh(từ bắt đầu đến ;) -->đặt trỏ chuột 
+-- vào dòng lệnh muốn chạy --> nhấn ctrl enter
+-- 001.sql
+-- --> ITEM_GROUP, ITEM, SIZE, ITEM_DETAIL
+
+-- 002.sql
+-- --> CUSTOMER
+
+-- 003.sql
+-- --> DEPARTMENT, EMPLOYEE
+
+-- 004.sql
+-- --> PAYMENT_METHOD, ORDER_STATUS
+
+-- 005.sql
+-- --> ORDER, BILL, ORDER_DETAIL, ORDER_STATUS_DETAIL
