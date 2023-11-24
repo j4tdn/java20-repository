9442f285c10f48c129fe917dc0f997e4dc8c4958
hongloan0115 <hongloan0115@gmail.com>
2023-11-24 19:21:09 +0700

    lesson16-tranthihongloan 24.11.2023

---
+++ lesson16-tranthihongloan/bai2-shopping-db-design/000.sql
@@ -0,0 +1,23 @@
+-- create database
+CREATE DATABASE java20_shopping CHAR SET utf8mb4;
+
+-- Để chạy câu lệnh
+-- 1. Bôi đậm đoạn code cần chạy --> nhấn nút execute
+-- 2. Để chạy 1 lệnh(từ bắt đầu đến ;) --> đặt trỏ chuột
+--    vào dòng lệnh muốn chạy --> nhấn ctrl enter
+
+
+-- 001.sql
+-- -->  ITEM_GROUP, ITEM, SIZE, ITEM_DETAIL
+
+-- 002.sql
+-- --> CUSTOMER
+
+-- 003.sql
+-- --> DEPARTMENT, EMPLOYEE
+
+-- 004.sql
+-- --> PAYMENT_METHOD, ORDER_STATUS
+
+-- 005.sql
+-- --> ORDER, BILL, ORDER_DETAIL, ORDER_STATUS_DETAIL
