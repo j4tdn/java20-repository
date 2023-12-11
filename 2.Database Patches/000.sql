92eefb0c5ab5f502593350b1f6d955d949fe3e58
Anh Nga <nguyenanhnga11@gmail.com>
2023-12-11 08:28:14 +0700

    SQL DML DEMO 11.12.2023

--- 2.Database Patches/000.sql
+++ 2.Database Patches/000.sql
@@ -1,16 +1,25 @@
-001.sql
--- -->ITEM_GROUP, ITEM, SIZE, ITEM_DETAIL
+-- create database
+CREATE DATABASE JAVA1920_SHOPPING CHAR SET utf8mb4;
 
-002.sql
--- -->CUSTOMER
+-- Để chạy câu lệnh
+-- 1. Bôi đậm đoạn code cần chạy --> nhấn nút execute
+-- 2. Để chạy 1 lệnh(từ bắt đầu đến ;) --> đặt trỏ chuột
+--    vào dòng lệnh muốn chạy --> nhấn ctrl enter
 
-003.sql
--- -->DEPARMENT, EMPLOYEE
+-- 001.sql
+-- --> ITEM_GROUP, ITEM, SIZE, ITEM_DETAIL
 
-004.sql
--- -->	PAYMENT_METHOD, ORDER_STATUS
+-- 002.sql
+-- --> CUSTOMER
 
-005.sql
--- -->ORDER, BILL, ORDER_DETAIL, ORDER_STATUS_DETAIL
+-- 003.sql
+-- --> DEPARTMENT, EMPLOYEE
 
+-- 004.sql
+-- --> PAYMENT_METHOD, ORDER_STATUS
 
+-- 005.sql
+-- --> ORDER, BILL, ORDER_DETAIL, ORDER_STATUS_DETAIL
+
+-- 006.sql
+-- --> RENAME column KEY to SIZE in table SIZE
