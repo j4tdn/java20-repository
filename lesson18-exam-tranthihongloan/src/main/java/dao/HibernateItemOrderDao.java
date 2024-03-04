1186488d386ad7e975566f7e49ad3b66b137ce4c
hongloan0115 <hongloan0115@gmail.com>
2024-03-04 21:59:19 +0700

    lesson18-exam 04.03.2024

--- lesson18-exam-tranthihongloan/src/main/java/dao/HibernateItemOrderDao.java
+++ lesson18-exam-tranthihongloan/src/main/java/dao/HibernateItemOrderDao.java
@@ -13,8 +13,8 @@ public class HibernateItemOrderDao extends BaseHibernateDao implements ItemOrder
 	
 			+ "SELECT it.ID " + ItemOrderDto.PROP_IO_ID + " ,\n"
 			+ "	      it.NAME " + ItemOrderDto.PROP_IO_NAME + ",\n"
-			+ "       CAST(o.CREATED_AT AS TIME) " + ItemOrderDto.PROP_IO_CREATED_TIME + ",\n"
-			+ "  FROM order o\n"
+			+ "       CAST(o.CREATED_AT AS TIME) " + ItemOrderDto.PROP_IO_CREATED_TIME + "\n"
+			+ "  FROM `order` o\n"
 			+ "  JOIN order_detail od\n"
 			+ "    ON o.ID = od.ORDER_ID\n"
 			+ "  JOIN item it\n"
@@ -27,6 +27,9 @@ public class HibernateItemOrderDao extends BaseHibernateDao implements ItemOrder
 		return openSession()
 			.createNativeQuery(Q_GET_ITEMS_ORDER_BY_DAY)
 			.setParameter("pcreatedtime", createdAt, StandardBasicTypes.STRING)
+			.addScalar(ItemOrderDto.PROP_IO_ID, StandardBasicTypes.INTEGER)
+			.addScalar(ItemOrderDto.PROP_IO_NAME, StandardBasicTypes.STRING)
+			.addScalar(ItemOrderDto.PROP_IO_CREATED_TIME, StandardBasicTypes.TIME)
 			.setResultTransformer(Transformers.aliasToBean(ItemOrderDto.class))
 			.getResultList();
 	}
