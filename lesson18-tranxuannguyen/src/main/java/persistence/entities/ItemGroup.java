0e30625ebd64d83169c2728e5d461d6188ffdd42
nuynt <tranxuannguyen732@gmail.com>
2024-03-04 21:59:20 +0700

    lesson18-tranxuannguyen 04.03.2024

--- lesson18-tranxuannguyen/src/main/java/persistence/entities/ItemGroup.java
+++ lesson18-tranxuannguyen/src/main/java/persistence/entities/ItemGroup.java
@@ -1,83 +1,37 @@
 package persistence.entities;
 
 import java.util.List;
-import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedNativeQueries;
-import javax.persistence.NamedNativeQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-
-/**
- * Sử dụng ORM framework để mapping class trong Java với table trong Database
- * + Tên class, attribute bất kỳ
- * + Tên table, column bất kỳ
- * + Mapping class với table name
- * + Mapping attributes với table columns
- * + Mapping constraints
- * 
- * Không bắt buộc phải mapping 1/ALL classes với tables
- * Khi nào cần truy vấn trả về dạng entity mình mới mapping
- * để đỡ phải set tay column với attribute 
- * 
- */
-
 @Entity
-@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Table(name = "item_group")
-
-//@NamedQueries(
-//	@NamedQuery(
-//			name = ItemGroup.Q_GET_ALL, 
-//			query = "FROM ItemGroup" /*JPQL*/)
-//)
-
-@NamedNativeQueries(
-	@NamedNativeQuery(
-		name = ItemGroup.Q_GET_ALL,
-		query = "SELECT * FROM item_group", /*Native Query*/
-		resultClass = ItemGroup.class)
-)
-
 public class ItemGroup {
-	
-	public static final String Q_GET_ALL = "Q_GET_ALL";
-	
-	// save ItemGroup into database table item_group
-	// setId(null)
-	// Java Layer --> OK
-	// Database Layer --> Exception
-	
-	// Prefer[optional]: Validation at Java Layer --> restrict pass wrong data/connect into database
-	
+
 	@Id
-	@Column(name = "ID", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	private Integer id;
-	
-	@Column(name = "NAME", nullable = false)
+
+	@Column(name = "NAME")
 	private String name;
-	
-	// @OneToMany: mặc định fetchType = LAZY
-	
-	@OneToMany(mappedBy = "group")
+
+	@OneToMany(mappedBy = "itemGroup", fetch = FetchType.LAZY)
 	private List<Item> items;
-	
-	/*
-	 * Hibernate required empty constructor
-	 */
-	public ItemGroup() {
-	}
 
 	public ItemGroup(Integer id, String name) {
 		this.id = id;
 		this.name = name;
+	}
+
+	public ItemGroup() {
 	}
 
 	public Integer getId() {
@@ -95,38 +49,18 @@ public class ItemGroup {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public List<Item> getItems() {
 		return items;
 	}
-	
+
 	public void setItems(List<Item> items) {
 		this.items = items;
 	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		
-		if (!(o instanceof ItemGroup)) {
-			return false;
-		}
-		
-		ItemGroup that = (ItemGroup)o;
-		
-		return getId() == that.getId();
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(getId());
-	}
-	
+
 	@Override
 	public String toString() {
 		return "ItemGroup [id=" + id + ", name=" + name + "]";
 	}
-	
+
 }
